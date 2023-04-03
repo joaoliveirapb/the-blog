08b55411f8b7e21f30bb854813173b08332fee10
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-04-03 19:16:16 -0300

    created content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover: 'https://d1fbh1knoaoilz.cloudfront.net/2022/12/What-s-new-in-Angular-15---Peerlist---cover.png',
+    title: 'Saiu a nova versão do Angular',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus doloribus accusamus asperiores ratione eius enim quae pariatur dignissimos repellendus odit.'
+  },
+  {
+    id: '2',
+    photoCover: 'https://thenewscc.com.br/wp-content/uploads/2023/04/TECH.png',
+    title: 'Itália proíbe ChatGPT, recorde da Tesla e NYT não quer verificado',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus doloribus accusamus asperiores ratione eius enim quae pariatur dignissimos repellendus odit.'
+  },
+  {
+    id: '3',
+    photoCover: 'https://thenewscc.com.br/wp-content/uploads/2023/03/Untitled-1.png',
+    title: 'Musk e outros líderes tech pedem uma pausa na AI',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus doloribus accusamus asperiores ratione eius enim quae pariatur dignissimos repellendus odit.'
+  },
+  {
+    id: '4',
+    photoCover: 'https://thenewscc.com.br/wp-content/uploads/2023/03/Untitled-55.png',
+    title: 'China faz empréstimos bilionários para recuperar outros países',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus doloribus accusamus asperiores ratione eius enim quae pariatur dignissimos repellendus odit.'
+  }
+]
